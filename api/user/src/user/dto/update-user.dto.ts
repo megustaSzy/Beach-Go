60989669fe744cea146218b207c4d62c0bbf27b0
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-05-09 21:18:23 +0700

    feat(validation): implement optional validation on UpdateUserDto

--- api/user/src/user/dto/update-user.dto.ts
+++ api/user/src/user/dto/update-user.dto.ts
@@ -1,4 +1,15 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  notelp?: string;
+}
