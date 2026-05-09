71ce47bb74e22d75ca65e25d41d377c4d5e23170
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-05-09 20:21:40 +0700

    feat(user): implement get all users with not found exception response

--- api/user/src/user/user.service.ts
+++ api/user/src/user/user.service.ts
@@ -1,15 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    const data = await this.prisma.user.findMany();
+
+    if (data.length === 0) {
+      throw new NotFoundException({
+        success: false,
+        message: process.env.NOT_FOUND_SAVE,
+        metadata: {
+          status: HttpStatus.NOT_FOUND,
+          total_data: data.length,
+        },
+      });
+    }
+
+    return {
+      success: true,
+      message: process.env.FOUND_SAVE,
+      metadata: {
+        status: HttpStatus.OK,
+        total_data: data.length,
+      },
+      data: data,
+    };
   }
 
   findOne(id: number) {
