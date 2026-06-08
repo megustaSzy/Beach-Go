7983484afda954740722a3308aac4a6ffc5e88ce
Azraa175 <fajarazriel17@gmail.com>
2026-06-08 11:56:46 +0700

    feat(mobile): add global application URL configurations

---
+++ mobile-user/constants/config.ts
@@ -0,0 +1,8 @@
+/**
+ * Global App Server Configuration constants
+ */
+export const SERVER_CONFIG = {
+  BACKEND_BASE_URL: 'http://10.0.2.2:3001',
+  TIMEOUT_MS: 10000,
+  APP_VERSION: '1.0.0',
+};
