79c1eb4f7c76166a278e5bd8f1e498cfe057b36d
Azraa175 <fajarazriel17@gmail.com>
2026-06-08 11:56:17 +0700

    feat(mobile): define application settings and key configuration constants

---
+++ mobile-user/utils/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Global configurations and constants
+ */
+export const APP_CONFIG = {
+  API_URL_ANDROID: 'http://10.0.2.2:3001',
+  API_URL_IOS: 'http://localhost:3001',
+  DEFAULT_LOCALE: 'id-ID',
+  APP_NAME: 'Beach-Go',
+  STORAGE_KEYS: {
+    USER: '@BeachGo:user',
+    BOOKMARKS: '@BeachGo:bookmarks',
+    THEME: '@BeachGo:theme',
+  }
+};
