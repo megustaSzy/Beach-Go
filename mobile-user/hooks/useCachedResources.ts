632cd2e8aa07e571e4db50b92a3ee1e1b0c82cae
Azraa175 <fajarazriel17@gmail.com>
2026-06-08 11:56:47 +0700

    feat(mobile): create custom useCachedResources preload hook

---
+++ mobile-user/hooks/useCachedResources.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
+
+/**
+ * Hook to pre-cache application font resources during start
+ */
+export function useCachedResources() {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
+        // Load fonts
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium,
+          Inter_600SemiBold,
+          Inter_700Bold,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return isLoadingComplete;
+}
