de991c4f712ea15186053a16b0853ebabdd5b415
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-06-13 10:58:35 +0700

    fix(gateway): install bcrypt and load environment variables to resolve missing jwt secret

--- api/gateway/src/main.ts
+++ api/gateway/src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors(); // Optional but good for gateway
+  app.enableCors();
   
   app.useGlobalPipes(
     new ValidationPipe({
