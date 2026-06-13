43857d9c9e329928b18b20bbec3162a9de8722f2
Deni Himawan <denihimawan595@gmail.com>
2026-06-13 23:25:02 +0700

    refactor: change name function dashboard

--- web-cms/src/app/dashboard/page.tsx
+++ web-cms/src/app/dashboard/page.tsx
@@ -1,9 +1,8 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-export default function Page() {
+export default function Dashboard() {
   return (
     <SidebarProvider
       style={
