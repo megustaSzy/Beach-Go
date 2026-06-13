d81ec1fae8b3242d980543137c8490beaf870e16
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-06-13 10:53:43 +0700

    feat(user): add regex validation for password and phone number in create user DTO

--- api/user/src/user/dto/create-user.dto.ts
+++ api/user/src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -9,8 +9,14 @@ export class CreateUserDto {
   email!: string;
 
   @MinLength(6)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]+$/, {
+    message: 'password must contain at least one uppercase letter, one lowercase letter, and one number',
+  })
   password!: string;
 
   @IsNotEmpty()
+  @Matches(/^(\+62|62|0)8[1-9][0-9]{6,10}$/, {
+    message: 'notelp must be a valid Indonesian phone number',
+  })
   notelp!: string;
 }
